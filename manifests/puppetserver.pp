b8ceeab21d2f8b1efbb731c3be836addf42509f7
Kristof Willaert <kristof@publiq.be>
2023-06-02 15:32:06 +0200

    Add deployment of publiq-infrastructure and publiq-appconfig to puppetserver role

--- manifests/puppetserver.pp
+++ manifests/puppetserver.pp
@@ -1,4 +1,6 @@
 class roles::puppetserver inherits roles::base {
 
   include profiles::puppet::puppetserver
+  include profiles::publiq::infrastructure::deployment
+  include profiles::publiq::appconfig::deployment
 }
