689035f25021d69ab84c2628fce5437d415b861a
Kristof Willaert <kristof@publiq.be>
2023-07-07 14:50:12 +0200

    Add uitdatabank::rdf profile to uitdatabank::rdf role

--- manifests/uitdatabank/rdf.pp
+++ manifests/uitdatabank/rdf.pp
@@ -2,5 +2,5 @@ class roles::uitdatabank::rdf inherits ::roles::base {
 
   include profiles::java
   include profiles::jena_fuseki
-  #include profiles::uitdatabank::rdf
+  include profiles::uitdatabank::rdf
 }
