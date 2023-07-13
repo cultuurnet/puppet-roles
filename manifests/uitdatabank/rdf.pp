e73bab11281d27c74d06cefcfbadcace0a28ea59
Kristof Willaert <kristof@publiq.be>
2023-07-13 15:34:19 +0200

    Make sure we list all profiles we allow parameters on in the role

--- manifests/uitdatabank/rdf.pp
+++ manifests/uitdatabank/rdf.pp
@@ -2,5 +2,6 @@ class roles::uitdatabank::rdf inherits ::roles::base {
 
   include profiles::java
   include profiles::jena_fuseki
+  include profiles::apache
   include profiles::uitdatabank::rdf
 }
--- manifests/uitpas/logs.pp
+++ manifests/uitpas/logs.pp
@@ -1,5 +1,6 @@
 class roles::uitpas::logs inherits roles::base {
 
+  include profiles::java
   include profiles::logstash
   include profiles::uitpas::cid_logs
 }
