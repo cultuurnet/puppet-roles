599b7a7ab41b9ad7ca7f13fc1ae9de582985efd1
Kristof Willaert <kristof@publiq.be>
2023-08-11 12:54:41 +0200

    Start filling the uit and uit::frontend roles

--- manifests/uit.pp
+++ manifests/uit.pp
@@ -1,3 +1,7 @@
 class roles::uit inherits ::roles::base {
 
+  include profiles::nodejs
+  include profiles::apache
+  include profiles::apache::vhost::generic
+  include profiles::deployment::uit::frontend
 }
