75a9f3b3cca6fae41500c0b479485018cc67fc06
Kristof Willaert <kristof@publiq.be>
2023-08-16 15:01:02 +0200

    Use uit::frontend profile instead of uit::frontend::deployment

--- manifests/uit/frontend.pp
+++ manifests/uit/frontend.pp
@@ -3,5 +3,5 @@ class roles::uit::frontend inherits ::roles::base {
   include profiles::nodejs
   include profiles::apache
   include profiles::apache::vhost::generic
-  include profiles::deployment::uit::frontend
+  include profiles::uit::frontend
 }
