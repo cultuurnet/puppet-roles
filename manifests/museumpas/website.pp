d2e43254c2a70c662bcd49eb6dd1a78ebfa8290b
Paul Herbosch <paul.herbosch@publiq.be>
2023-09-26 13:22:51 +0200

    update museumpas website role: use updated profiles

--- manifests/museumpas/website.pp
+++ manifests/museumpas/website.pp
@@ -1,9 +1,10 @@
 class roles::museumpas::website inherits ::roles::base {
 
-  include varnish
+  include redis
   include profiles::php
   include profiles::apache
-  include profiles::deployment::mpm::website
+  include profiles::meilisearch
+  include profiles::museumpas::website
   include profiles::museumpas::automatic_renewal_mail
   include profiles::museumpas::automatic_mandates
 }
