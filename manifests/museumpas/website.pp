4077e4eab31e523813b80eb37cbf014c01e32257
Paul Herbosch <paul.herbosch@publiq.be>
2023-09-28 19:00:19 +0200

    [MPM-702] move redis and meilisearch profiles to museumpas website deployment profile

--- manifests/museumpas/website.pp
+++ manifests/museumpas/website.pp
@@ -1,9 +1,7 @@
 class roles::museumpas::website inherits ::roles::base {
 
-  include redis
   include profiles::php
   include profiles::apache
-  include profiles::meilisearch
   include profiles::museumpas::website
   include profiles::museumpas::automatic_renewal_mail
   include profiles::museumpas::automatic_mandates
