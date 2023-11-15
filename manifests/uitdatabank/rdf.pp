ab087ba422b19128b8dc110536ee5130033152e3
Kristof Willaert <kristof@publiq.be>
2023-11-15 15:18:26 +0100

    Remove Jena Fuseki from UiTdatabank RDF role

--- manifests/uitdatabank/rdf.pp
+++ manifests/uitdatabank/rdf.pp
@@ -1,7 +1,5 @@
 class roles::uitdatabank::rdf inherits ::roles::base {
 
-  include profiles::java
-  include profiles::jena_fuseki
   include profiles::apache
   include profiles::uitdatabank::rdf
 }
