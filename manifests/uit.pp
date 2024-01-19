05ca0ec7ce05d727b7fbb6ebe955e92f867a2add
Kristof Willaert <kristof@publiq.be>
2024-01-19 14:43:12 +0100

    Add redis and mysql::server profile to uit::api role

--- manifests/uit.pp
+++ manifests/uit.pp
@@ -2,6 +2,8 @@ class roles::uit inherits ::roles::base {
 
   include profiles::nodejs
   include profiles::apache
+  include profiles::redis
+  include profiles::mysql::server
   include profiles::uit::frontend
   include profiles::uit::api
   include profiles::newrelic_infra
--- manifests/uit/api.pp
+++ manifests/uit/api.pp
@@ -2,6 +2,8 @@ class roles::uit::api inherits ::roles::base {
 
   include profiles::nodejs
   include profiles::apache
+  include profiles::redis
+  include profiles::mysql::server
   include profiles::uit::api
   include profiles::newrelic_infra
 }
