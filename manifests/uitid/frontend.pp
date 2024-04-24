53eddee7797f4ac264b30d0a1150c747fbd91dcd
Kristof Willaert <kristof@publiq.be>
2024-04-24 14:28:32 +0200

    Remove profiles::uitid::frontend_auth0 from uitid::frontend role

--- manifests/uitid/frontend.pp
+++ manifests/uitid/frontend.pp
@@ -5,5 +5,4 @@ class roles::uitid::frontend inherits ::roles::base {
   include profiles::redis
   include profiles::uitid::frontend
   include profiles::uitid::frontend_api
-  include profiles::uitid::frontend_auth0
 }
