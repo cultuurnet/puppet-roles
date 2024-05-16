53c29bc8168e33f54d56cc7907fe77b7a4891bd2
Kristof Willaert <kristof@publiq.be>
2024-05-16 16:23:58 +0200

    Add profiles::python to jenkins::agent role

--- manifests/jenkins/agent.pp
+++ manifests/jenkins/agent.pp
@@ -2,9 +2,9 @@ class roles::jenkins::agent inherits roles::base {
 
   include profiles::java
   include profiles::jenkins::node
-  #include profiles::puppet::puppetdb::cli
   include profiles::terraform
   include profiles::php
   include profiles::nodejs
+  include profiles::python
   include profiles::docker
 }
