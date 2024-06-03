b2661417c73b0ca6f490fdd0d07fc3640ed3509f
Paul Herbosch <paul.herbosch@publiq.be>
2024-06-03 17:09:00 +0200

    [MPM-1066] Add mpm patner website profile to MPM website role

--- manifests/museumpas/website.pp
+++ manifests/museumpas/website.pp
@@ -3,6 +3,7 @@ class roles::museumpas::website inherits ::roles::base {
   include profiles::php
   include profiles::apache
   include profiles::museumpas::website
+  include profiles::museumpas::partner_website
   include profiles::museumpas::automatic_renewal_mail
   include profiles::museumpas::automatic_mandates
 }
