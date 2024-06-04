2da322291fd2d98bf5f91028889721244d60bdfa
Paul Herbosch <paul.herbosch@publiq.be>
2024-06-04 15:56:30 +0200

    add mpm partner website

--- manifests/museumpas/website_cache.pp
+++ manifests/museumpas/website_cache.pp
@@ -3,6 +3,7 @@ class roles::museumpas::website_cache inherits ::roles::base {
   include profiles::php
   include profiles::apache
   include profiles::museumpas::website
+  include profiles::museumpas::partner_website
   include profiles::museumpas::automatic_renewal_mail
   include profiles::museumpas::automatic_mandates
   include profiles::redis
