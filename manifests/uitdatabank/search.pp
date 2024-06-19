c005c4e462b086bab3fa747e4c4056ab88d11fe2
Kristof Willaert <kristof@publiq.be>
2024-06-19 09:29:19 +0200

    Disable elasticdump_to_gcs in uitdatabank::search role for now

--- manifests/uitdatabank/search.pp
+++ manifests/uitdatabank/search.pp
@@ -2,9 +2,9 @@ class roles::uitdatabank::search inherits ::roles::base {
 
   include profiles::php
   include profiles::java
-  include profiles::nodejs
+  #include profiles::nodejs
   include profiles::apache
   include profiles::elasticsearch
   include profiles::uitdatabank::search_api
-  include profiles::uitdatabank::elasticdump_to_gcs
+  #include profiles::uitdatabank::elasticdump_to_gcs
 }
