091c82a936cc6e62b8269465fd17e36fd7fa1e54
Kristof Willaert <kristof@publiq.be>
2024-06-19 10:58:07 +0200

    Remove declarations of uitdatabank::elasticdump_to_gcs profile, as it will be refactored

--- manifests/uitdatabank/search.pp
+++ manifests/uitdatabank/search.pp
@@ -2,9 +2,7 @@ class roles::uitdatabank::search inherits ::roles::base {
 
   include profiles::php
   include profiles::java
-  #include profiles::nodejs
   include profiles::apache
   include profiles::elasticsearch
   include profiles::uitdatabank::search_api
-  #include profiles::uitdatabank::elasticdump_to_gcs
 }
