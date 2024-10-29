0e6437087e24ff8611e7e3d3e1f6f30975b27075
Kristof Willaert <kristof@publiq.be>
2024-10-29 15:39:51 +0100

    Add websocket server to UiTdatabank web role

--- manifests/uitdatabank/web.pp
+++ manifests/uitdatabank/web.pp
@@ -1,4 +1,7 @@
 class roles::uitdatabank::web inherits ::roles::base {
 
+  include profiles::nodejs
+  include profiles::redis
   include profiles::uitdatabank::entry_api
+  include profiles::uitdatabank::websocket_server
 }
