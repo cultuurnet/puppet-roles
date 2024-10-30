50d07fde833269b3766f33c2b70d0c02772e329b
Kristof Willaert <kristof@publiq.be>
2024-10-30 13:19:26 +0100

    Add apache profile to uitdatabank::web role for clarity

--- manifests/uitdatabank/web.pp
+++ manifests/uitdatabank/web.pp
@@ -2,6 +2,7 @@ class roles::uitdatabank::web inherits ::roles::base {
 
   include profiles::nodejs
   include profiles::redis
+  include profiles::apache
   include profiles::uitdatabank::entry_api
   include profiles::uitdatabank::websocket_server
 }
