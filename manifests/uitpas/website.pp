11a2a9914f63ccc60519857d84655237b3fed6c4
Vincent Verbist <vincent.verbist@publiq.be>
2025-01-31 10:25:16 +0100

    Add profile of uitpas-cid-web

--- manifests/uitpas/website.pp
+++ manifests/uitpas/website.pp
@@ -6,4 +6,5 @@ class roles::uitpas::website inherits ::roles::base {
   include profiles::uitpas::website::frontend
   include profiles::uitpas::website::api
   include profiles::uitpas::website::elasticsearch
+  include profiles::uitpas::cid_web
 }
