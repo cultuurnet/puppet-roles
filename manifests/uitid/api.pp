dd16c1a31b2f4e8ea18f7f0bae836b5fb9c9854b
Kristof Willaert <kristof@publiq.be>
2025-10-30 15:57:07 +0100

    Use local MySQL server on all UiTiD API environments

--- manifests/uitid/api.pp
+++ manifests/uitid/api.pp
@@ -2,9 +2,6 @@ class roles::uitid::api inherits ::roles::base {
 
   include profiles::java
   include profiles::glassfish
+  include profiles::mysql::server
   include profiles::uitid::api
-
-  if $environment == 'acceptance' {
-    include profiles::mysql::server
-  }
 }
