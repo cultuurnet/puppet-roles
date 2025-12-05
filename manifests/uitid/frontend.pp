03ad2e015e2f7bba4141a6c76c1d948056047932
Kristof Willaert <kristof@publiq.be>
2025-12-05 11:28:00 +0100

    Remove unused profile UiTiD frontend API from UiTiD frontend role

--- manifests/uitid/frontend.pp
+++ manifests/uitid/frontend.pp
@@ -4,5 +4,4 @@ class roles::uitid::frontend inherits ::roles::base {
   include profiles::apache
   include profiles::redis
   include profiles::uitid::frontend
-  include profiles::uitid::frontend_api
 }
