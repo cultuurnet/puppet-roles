62400653289342f0ba34246b7822eaf01ccb5e45
Kristof Willaert <kristof@publiq.be>
2026-05-18 20:22:36 +0200

    Use data_integration profile for UiTdatabank search API

--- manifests/uitdatabank/search.pp
+++ manifests/uitdatabank/search.pp
@@ -6,5 +6,5 @@ class roles::uitdatabank::search inherits ::roles::base {
   include profiles::redis
   include profiles::elasticsearch
   include profiles::uitdatabank::search_api
-  include profiles::uitdatabank::search_api::elasticdump_to_gcs
+  include profiles::uitdatabank::search_api::data_integration
 }
